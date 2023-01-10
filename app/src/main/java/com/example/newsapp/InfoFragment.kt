962fe17d50735595b15cf0cd86410494cc296d1c
michalgwo <114614618+michalgwo@users.noreply.github.com>
2023-01-10 17:09:02 +0100

    Created fragments and navigation

---
+++ app/src/main/java/com/example/newsapp/InfoFragment.kt
@@ -0,0 +1,15 @@
+package com.example.newsapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class InfoFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_info, container, false)
+    }
+}
--- app/src/main/java/com/example/newsapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/presentation/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.newsapp.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.newsapp.R
+import com.example.newsapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        createNavigation()
+    }
+
+    private fun createNavigation() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
